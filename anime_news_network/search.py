1a0122fc192188704ae5b733d272a007b14b1fe6
Nekmo <contacto@nekmo.com>
2016-09-17 05:13:42 +0200

    is_id to function.

--- anime_news_network/search.py
+++ anime_news_network/search.py
@@ -13,9 +13,12 @@ searchs = []
 last_search = 0
 
 
+def is_id(query):
+    return isinstance(query, int)
+
+
 def search(query, type='title'):
     from anime_news_network.results import Results
-    is_id = isinstance(query, int)
     wait_to = searchs[-1] if searchs else None
     my_event = Event()
     searchs.append(my_event)
@@ -23,7 +26,7 @@ def search(query, type='title'):
         wait_to[-1].wait(THREAD_TIMEOUT)
     if last_search and time.time() - last_search < DELAY:
         time.sleep(max(DELAY - time.time() - last_search, 0))
-    query = {type: str(query) if is_id else '~{}'.format(query)}
+    query = {type: str(query) if is_id(query) else '~{}'.format(query)}
     data = requests.get(URL, query)
     my_event.set()
     print(Results(data.text).to_json())
